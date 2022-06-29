fd5ec3315a593d1df841a592e2cfe04c0ac6d14e
NaliniNN <n.nalini@capgemini.com>
2022-06-29 17:10:37 +0530

    memo-added

---
+++ memo-app/src/Component/parent.jsx
@@ -0,0 +1,34 @@
+import React ,{Component} from "react";
+import Child from "./child";
+import ChildOne from "./childOne";
+
+
+class Parent extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      name : "Javascript" //1
+    }
+  }
+
+  componentDidMount() {
+    console.log("componentDIDMount");
+    setInterval(()=> {
+      this.setState({
+        name : "React" //name: this.state.name
+      })
+    },2000);
+  }
+
+  render(){
+    console.log("Parent component Render");
+    return(
+      <>
+      <h1>Parent Component Name : {this.state.name} </h1>
+      <Child name={this.state.name} />
+      <ChildOne />
+      </>
+    );
+  }
+}
+export default Parent;
